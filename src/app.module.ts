d58c747ad5a28acc6b7efe89bc8f24c7531aa3b1
xLegitGhost <contacto.urielramirez@gmail.com>
2026-09-14 19:25:55 -0600

    Refactor code structure for improved readability and maintainability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import  { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { EmployeesModule } from './employees/employees.module.js';
 import { ProductsModule } from './products/products.module.js';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [EmployeesModule, ProductsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.host ?? "localhost",
+    port: process.env.dbPort ? parseInt(process.env.dbPort) : 5432,
+    username: "postgres",
+    password: process.env.dbPass,
+    database: process.env.dbName,
+  }) ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,16 @@
+import {IsInt, IsNumber, IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
+
 export class CreateProductDto {
+    @IsUUID("4")
+    @IsString()
+    @IsOptional()
     productId: string;
+    @IsString() @MaxLength(100)
     productName: string;
+    @IsNumber()
     price: number;
+    @IsInt()
     countSeal: number;
+    @IsUUID("4") @IsString()
     provider: string;
 }
